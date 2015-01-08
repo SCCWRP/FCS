6cd0de2ad0aae66ac96c478c94e500dc4882581c
Paul Smith <pauls@sccwrp.org>
2015-01-08 13:41:42 -0800

    local storage and sd2

---
+++ js/views/StorageView.js
@@ -0,0 +1,68 @@
+var StorageView = Backbone.View.extend({
+	el: '#content',
+	template:_.template($('#tpl-storage-details').html()),
+	initialize: function(){
+		alert("initialize StorageView");
+		var self = this;
+		fileSystem.root.getFile("test.txt", {create:true}, this.fileAppend, this.onError);
+	},
+  	fileAppend: function(f){
+		alert("fileAppend");
+  		//alert(f.fullPath);
+    		f.createWriter(function(writerOb) {
+			writerOb.onwrite=function() {
+            			app.showContent("Done writing to file.<p/>");
+        	  	}
+        	//go to the end of the file...
+        	writerOb.seek(writerOb.length);
+		var localSave = getLocalData("local","save");
+        	writerOb.write(localSave)
+    		})
+        },
+  	getLocalData: function(a,t){
+     		//alert("a: "+a);
+     		//alert("t: "+t);
+     		var localSave;
+     		var prevStorage = window.localStorage.getItem("fcs-keys");
+      		alert("prevStorage: "+prevStorage); 
+     		if (prevStorage != null){
+	     		alert("The following session keys are saved " + prevStorage);
+	     		var keysArray = prevStorage.split(',');
+	     		//var connectionStatus = navigator.onLine ? 'online' : 'offline';
+	     		//if(connectionStatus != "offline") {
+	     		var currentKey; // currentKey = sessionid
+	     		var loopNum=keysArray.length;
+	     		alert("Should loop " + loopNum + " times");
+	     		for(var i=0; i<loopNum; i++){
+		     		//alert("Loop number " +  i + "");
+		     		currentKey = keysArray.pop();
+		     		//alert("currentKey: "+currentKey);
+		     		currentTime = currentKey.split('-');
+		     		//alert("currentTimestamp: "+currentTime[2]);
+		     		var read =  window.localStorage.getItem(currentKey);
+		     		if(a=="local"){
+     					//alert("a: "+a);
+					localSave += read;	
+		     		}
+		     		//alert("Read Session: "+ read);
+		     		if(a=="remote"){
+					alert("read: "+read);
+		     			app.submitRemote(read,currentTime[2]);
+		     		}
+			     	//to_submit = read.split(',');
+			     	//n = oldKey.split('_')[1];
+	     		} // close for
+	     		if(a=="local"){
+   				alert("a Save: ");
+				return localSave;
+	     		}
+		}
+	},
+	render: function(){
+		$("#landing").hide();
+		$(headerView.el).show();
+		$('#question').html("Camera");
+		$(this.el).html("");
+		//$(this.el).html(this.template({FAquestions: this.FAQcol.toJSON() }));	
+	}
+});
