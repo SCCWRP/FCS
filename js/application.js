904c5898a7ce2b72379ba3d1ae92a8544b14e254
Paul Smith <pauls@sccwrp.org>
2015-01-13 11:00:19 -0800

    local storage and sd11

--- js/application.js
+++ js/application.js
@@ -157,7 +157,7 @@ var app = {
     		document.addEventListener("deviceready", function(){
 			alert("isDevice deviceready");
 			app.onDeviceReady();
-			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, this.onFSSuccess, this.onError);
+			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, app.onFSSuccess, app.onError);
 		},true);
 	} else {
 		app.onDeviceReady();
