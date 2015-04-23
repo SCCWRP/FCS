77e9c7f92be16aaa0d1373e95bad6c6677e27c54
Paul Smith <pauls@sccwrp.org>
2015-04-23 14:03:10 -0700

    adjustment to font sizes for mobile select menu2

--- js/application.js
+++ js/application.js
@@ -361,7 +361,6 @@ var app = {
 	$.mobile.linkBindingEnabled = false;
 	$.mobile.hashListeningEnabled = false;
 	$.mobile.pushStateEnabled = false;
-	$.mobile.selectmenu.prototype.options.nativeMenu = false;
 	//app.bindEvents();
 	Backbone.history.start({pushState: true, hashChange: false});
 	// check network status
